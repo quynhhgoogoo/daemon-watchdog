0efc202e5c1592704eb751b6e6725f754c225b9d
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2020-10-07 00:52:13 +0300

    start daemon

--- daemon.sh
+++ daemon.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-#Initialize time interval and log file's path
-time_interval=30
-log='/tmp/daemon.log'
+#Initialize script file and PID for daemon
+script_file='./record_daemon.sh'
+pid_file=${script_file}.pid
 
-#Create a loop
-while true
+#Start daemon
+startDaemon(){
+    echo -n "Start running process as a daemon\n"
+    if [ -f $pid_file ]; then
+        pid_content = `cat $pid_file`
+        echo "%s already running" "$pid_content"
+        exit 2;
+    else
+        $script_file & pid_content=$!
+        echo "Run sucessfully at %s" "$pid_content"
+        return 0
+    fi
+}
 
-do
-	#Get current date and time
-	date_and_time="$(date +'%d-%m-%Y %H:%M:%S')"
-	
-	#Record inside log file
-	printf "%s Mock daemon is still running\n" "$date_and_time" >> $log
-	
-	sleep $time_interval
-done
+startDaemon
---
+++ record_daemon.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Initialize time interval and log file's path
+time_interval=30
+log='/tmp/daemon.log'
+
+#Create a loop
+while true
+
+do
+	#Get current date and time
+	date_and_time="$(date +'%d-%m-%Y %H:%M:%S')"
+	
+	#Record inside log file
+	printf "%s Mock daemon is still running\n" "$date_and_time"
+	printf "%s Mock daemon is still running\n" "$date_and_time" >> $log
+	
+	sleep $time_interval
+done
