b8986ea54830ef301ec859f6155d32b9e53c2244
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2020-10-07 18:31:40 +0300

    watchdog check if daemon process is running

--- watchdog.sh
+++ watchdog.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 #Initialize log's path and time interval
+script_file='./record_daemon.sh'
+daemon="./daemon.sh"
+
+pid_file=${script_file}.pid
+
 watchdog_log='/tmp/watchdog.log'
 t1=10
 t2=300      #5 minutes
@@ -8,3 +13,22 @@ t2=300      #5 minutes
 #Startup
 printf "Watchdog started\n" >> $watchdog_log
 
+#First time interval: check if mock daemon is still running on system
+while true
+do
+    #If daemon process is running
+    if [ -f $pid_file ]; then
+        pid_content= `cat $pid_file`
+        echo -n "Daemon is running at" "$pid_content"
+        message="Daemon started"
+        echo $message > $watchdog_log
+        exit 1
+    else
+        pid_content= `cat $pid_file`
+        echo -n "Daemon is not running. Restart the daemon"
+        $daemon start
+        echo -n "Daemon is running with pid: " "$pid_content"
+        exit 0
+    fi
+    sleep $t1
+done
